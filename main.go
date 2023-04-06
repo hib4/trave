c112c71f2fdbd5e6c09dde9365bb073c04bf897f
hib4 <hiba.kudus@gmail.com>
2023-04-06 10:19:04 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]interface{}{"message": "Hello World!"})
+	}).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":80", r))
+}
